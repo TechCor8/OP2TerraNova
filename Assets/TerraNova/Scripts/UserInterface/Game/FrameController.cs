4e26a0f4602e977e7a0cd077185f20b402342717
TechCor8 <48194787+TechCor8@users.noreply.github.com>
2020-07-19 10:24:53 -0400

    Added QuitPopup and FrameController
    User can minimize, change windowed mode, and close with confirmation through the game frame.

---
+++ Assets/TerraNova/Scripts/UserInterface/Game/FrameController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using TerraNova.UserInterface.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TerraNova.UserInterface.Game
+{
+	/// <summary>
+	/// Controls buttons on the frame.
+	/// </summary>
+	public class FrameController : MonoBehaviour
+	{
+		[SerializeField] private GameObject _Focus		= default;
+		[SerializeField] private GameObject _NoFocus	= default;
+
+		[SerializeField] private Button _BtnMaximize	= default;
+		[SerializeField] private Button _BtnWindowed	= default;
+
+		private float _OriginalTimeScale;
+
+
+		private void Awake()
+		{
+			RefreshFocus();
+			RefreshMaximizeButton();
+		}
+
+		public void OnClick_Close()
+		{
+			// Pause game
+			_OriginalTimeScale = Time.timeScale;
+			Time.timeScale = 0;
+
+			// Confirm quit
+			ConfirmPopup.CreateFromResource("UserInterface/Game/QuitPopup", OnQuitPopupClosed);
+		}
+
+		private void OnQuitPopupClosed(bool didConfirm)
+		{
+			if (didConfirm)
+				Application.Quit();
+			else
+			{
+				// Unpause
+				Time.timeScale = _OriginalTimeScale;
+			}
+		}
+
+		public void OnClick_Maximize()
+		{
+			Screen.fullScreen = !Screen.fullScreen;
+
+			RefreshMaximizeButton();
+		}
+
+#if UNITY_STANDALONE_WIN
+		[DllImport("user32.dll")] private static extern bool ShowWindow(System.IntPtr hwnd, int nCmdShow);
+		[DllImport("user32.dll")] private static extern System.IntPtr GetActiveWindow();
+ 
+		public void OnClick_Minimize()
+		{
+			ShowWindow(GetActiveWindow(), 2);
+		}
+#else
+		public void OnClick_Minimize()
+		{
+			Debug.LogWarning("Minimize not supported on this platform!");
+		}
+#endif
+
+		private void Update()
+		{
+			// Detect full screen change
+			if (Screen.fullScreen != _BtnWindowed.gameObject.activeSelf)
+			{
+				RefreshMaximizeButton();
+			}
+		}
+
+		private void OnApplicationFocus(bool focus)
+		{
+			RefreshFocus();
+		}
+
+		private void RefreshFocus()
+		{
+			_Focus.gameObject.SetActive(Application.isFocused);
+			_NoFocus.gameObject.SetActive(!Application.isFocused);
+		}
+
+		private void RefreshMaximizeButton()
+		{
+			_BtnMaximize.gameObject.SetActive(!Screen.fullScreen);
+			_BtnWindowed.gameObject.SetActive(Screen.fullScreen);
+		}
+	}
+}
---
+++ Assets/TerraNova/Scripts/UserInterface/Generic/ConfirmPopup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace TerraNova.UserInterface.Generic
+{
+	/// <summary>
+	/// Represents a confirmation popup.
+	/// </summary>
+	public class ConfirmPopup : MonoBehaviour
+	{
+		public delegate void OnConfirmCallback(bool didConfirm);
+
+		private OnConfirmCallback _OnClosedCB;
+
+
+		public static ConfirmPopup CreateFromResource(string prefabPath, OnConfirmCallback onClosedCB)
+		{
+			GameObject goPopup = Instantiate(Resources.Load<GameObject>(prefabPath));
+			ConfirmPopup popup = goPopup.GetComponent<ConfirmPopup>();
+
+			popup._OnClosedCB = onClosedCB;
+
+			return popup;
+		}
+
+		public void OnClick_Yes()
+		{
+			_OnClosedCB?.Invoke(true);
+
+			Destroy(gameObject);
+		}
+
+		public void OnClick_No()
+		{
+			_OnClosedCB?.Invoke(false);
+
+			Destroy(gameObject);
+		}
+	}
+}
